b9d9ca30a872c80e517ec68184295c7c00590e31
sssteiny21 <sssteiny21@gmail.com>
2016-12-12 18:01:30 -0500

    added stylefolder

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 8080;
 var app = express(); // Create an application object
 
 // app.use are executed for every request the server receives
-app.use(express.static('public')); // If a URL that is requested matches a filepath that is INSIDE of the public folder.  Express will automatically send that file down to the browser
+app.use(express.static('Public')); // If a URL that is requested matches a filepath that is INSIDE of the public folder.  Express will automatically send that file down to the browser
 
 app.listen(PORT, ()=>{
   console.log(`Server running on port ${PORT}`);
